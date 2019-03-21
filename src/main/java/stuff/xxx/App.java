5f528a402fd55ab1f890d83aae5e82389c22e268
xxx <xxx@xxxs-MBP.fios-router.home>
2019-03-20 23:32:45 -0400

    minor updates to controller

--- src/main/java/stuff/xxx/App.java
+++ src/main/java/stuff/xxx/App.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import javax.servlet.ServletInputStream;
 import org.apache.commons.io.IOUtils;
+import org.checkerframework.common.reflection.qual.GetMethod;
 import spark.Spark;
 
 public class App {
@@ -11,14 +12,25 @@ public class App {
   public static void main(String[] args) {
 
     Spark.port(8080);
+    
     Spark.get("/", (request, response) -> "Hello world " + new Date());
+    
     Spark.get("/health", (request, response) -> "ok");
+    
     Spark.post("/", (request, response) -> {
       
-      String data = IOUtils.toString(request.raw().getInputStream(), StandardCharsets.UTF_8);
-      System.out.println(data);
+      String data = null;
+      
+      try { 
+        data = IOUtils.toString(request.raw().getInputStream(), StandardCharsets.UTF_8);
+        System.out.println(data);
+      }
+      catch(Exception e) {
+        System.out.println("Failure reading inputstream : " + e.getMessage());
+      }
       
       return "Server says : " + data;
+      
     });
   }
 }
